2a7bc993289ea6b54acf86c7e47e9ad853997953
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2020-05-04 10:00:10 +0300

    Build: Add `publishedLibrary` configuration helpers

--- buildSrc/src/main/kotlin/publishedConfigurations.kt
+++ buildSrc/src/main/kotlin/publishedConfigurations.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
 import org.gradle.api.Action
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.NamedDomainObjectProvider
@@ -6,11 +11,6 @@ import org.gradle.api.artifacts.Dependency
 import org.gradle.api.artifacts.ExternalModuleDependency
 import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.kotlin.dsl.accessors.runtime.addDependencyTo
-
-/*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
- */
 
 val NamedDomainObjectContainer<Configuration>.publishedRuntime: NamedDomainObjectProvider<Configuration> get() = named("publishedRuntime")
 
@@ -34,4 +34,16 @@ fun DependencyHandler.publishedCompile(
     dependencyConfiguration: Action<ExternalModuleDependency>
 ): ExternalModuleDependency =
     addDependencyTo(this, "publishedCompile", dependencyNotation, dependencyConfiguration)
+
+
+val NamedDomainObjectContainer<Configuration>.publishedLibrary: NamedDomainObjectProvider<Configuration> get() = named("publishedLibrary")
+
+fun DependencyHandler.publishedLibrary(dependencyNotation: Any): Dependency? =
+    add("publishedLibrary", dependencyNotation)
+
+fun DependencyHandler.publishedLibrary(
+    dependencyNotation: String,
+    dependencyConfiguration: Action<ExternalModuleDependency>
+): ExternalModuleDependency =
+    addDependencyTo(this, "publishedLibrary", dependencyNotation, dependencyConfiguration)
 
